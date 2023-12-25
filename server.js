c3d84ae224155118df5fc540cd78967ca2716c5f
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-25 18:40:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3002;
 
 // Enable CORS for all routes
 app.use(cors());
 
 app.get('/biryani', (req, res) => {
   res.json([
-    { name: 'Chicken Biryani', price: '10' },
-    { name: 'Mutton Biryani', price: '12' },
+    { name: 'Chicken Pizza', price: '10' },
+    { name: 'Veg Pizza', price: '12' },
     // More items...
   ]);
 });
 
 // If you want to handle the root path as well and avoid "Cannot GET /"
 app.get('/', (req, res) => {
-  res.send('Welcome to the Biryani Service');
+  res.send('Welcome to the Pizza Service');
 });
 
 app.listen(port, () => {
